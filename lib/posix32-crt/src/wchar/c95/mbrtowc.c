eb04718ab672323f26fdbdf77d4ed79aadad7147
Kirill Makurin <maiddaisuki@outlook.com>
2026-03-02 18:24:36 +0900

    posix32-crt: mbrtowc: ensure that no output is produced on error
    Since `wchar_t` is UTF-16 on Windows, we use `mbrtoc16` to implement `mbrtowc`,
    which unlike `mbrtoc16` is not capable of handling UTF-16 Surrogate Pairs.
    
    It is possible that `mbstate_t` object passed to `mbrtowc` will cause
    underlying `mbrtoc16` output the high surrogate; this is not a valid state that
    can normally occur with `mbrtowc`. When this occurs, `mbrtowc` sets `errno` to
    `EINVAL` and returns `(size_t)-1` to indicate an error.
    
    Currently, this results in the high surrogate produced by `mbrtoc16` being
    written to buffer passed to `mbrtowc`. This behavior is undesired since
    no output should be produced when an error occurs.
    
    To avoid this, use an internal buffer to hold intermediate results.

--- lib/posix32-crt/src/wchar/c95/mbrtowc.c
+++ lib/posix32-crt/src/wchar/c95/mbrtowc.c
@@ -25,7 +25,20 @@ size_t p32_private_mbrtowc_l (
 ) {
   assert (state != NULL);
 
-  const size_t length = locale->Functions.F_mbrtoc16 (wc, mbc, count, state, &locale->Charset);
+  /**
+   * Converted wide character.
+   *
+   * Unlike `mbrtoc16`, `mbrtowc` cannot handle UTF-16 Surrogate Pairs.
+   *
+   * If we consumed multibyte sequence which would produce UTF-16
+   * Surrogate Pair, we must ensure that we do not produce invalid UTF-16
+   * by writing only the low or high surrogate to `wc`.
+   *
+   * CRT's `mbrtowc` stores U+FFFD in such case and we follow this behavior.
+   */
+  wchar_t u16[2] = {WEOF, WEOF};
+
+  const size_t length = locale->Functions.F_mbrtoc16 (&u16[0], mbc, count, state, &locale->Charset);
 
   /**
    * `state` is conversion state from `mbrtoc8`, `mbrtoc16` or `mbrtoc32`.
@@ -43,25 +56,44 @@ size_t p32_private_mbrtowc_l (
   }
 
   /**
-   * We have consumed multibyte sequence, which would produce
-   * UTF-16 Surrogate pair.
-   *
-   * `wcrtomb` provides no mechanism to handle UTF-16 surrogate pairs.
+   * Conversion produced single non-surrogate UTF-16 Code Unit.
    */
-  if (!p32_mbsinit (state)) {
-    size_t ret = locale->Functions.F_mbrtoc16 (NULL, NULL, 0, state, &locale->Charset);
+  if (p32_mbsinit (state)) {
+    assert (!IS_HIGH_SURROGATE (u16[0]) && !IS_LOW_SURROGATE (u16[0]));
 
-    assert (ret == (size_t) -3);
-    assert (p32_mbsinit (state));
-
-    if (wc != NULL) {
-      *wc = 0xFFFD;
+    /**
+     * If `mbc` is `NULL`, then `wc` is ignored.
+     */
+    if (wc != NULL && mbc != NULL) {
+      *wc = u16[0];
     }
 
-    UNREFERENCED_PARAMETER (ret);
+    return length;
+  }
+
+  assert (mbc != NULL);
+
+  /**
+   * We have consumed multibyte sequence which would produce
+   * UTF-16 Surrogate pair.
+   *
+   * Call `mbrtoc16` again to update `state`.
+   * After this call `u16` must contain UTF-16 Surrogate Pair.
+   *
+   * If `wc` is not null, store U+FFFD.
+   */
+  size_t ret = locale->Functions.F_mbrtoc16 (&u16[1], "", 0, state, &locale->Charset);
+
+  assert (ret == (size_t) -3);
+  assert (p32_mbsinit (state));
+  assert (IS_SURROGATE_PAIR (u16[0], u16[1]));
+
+  if (wc != NULL) {
+    *wc = 0xFFFD;
   }
 
   return length;
+  UNREFERENCED_PARAMETER (ret);
 }
 
 static void P32LocaleFunction_mbrtowc (LocaleFunctions *functions, Charset *charset) {
