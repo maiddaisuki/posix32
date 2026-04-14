3244ecb04b936083c0446b6ad0e8d79e81459960
Kirill Makurin <maiddaisuki@outlook.com>
2026-04-15 00:12:06 +0900

    posix32-core: runtime.c: use runtime checks
    Add infrastructure required to dynamically lookup functions at runtime.
    
    Implementation of `p32_terminate` function:
    
    If library is configured for Windows Vista or older, try to lookup
    `RaiseFailFastException` at runtime and use it for implementation.
    
    If `RaiseFailFastException` is not available, use `RaiseException` to raise
    a non-continuable exception, falling back to `TerminateProcess` if necessary.

--- lib/posix32-core/src/runtime.c
+++ lib/posix32-core/src/runtime.c
@@ -20,6 +20,8 @@
 #include "config-internal.h"
 #endif
 
+#include <assert.h>
+#include <pthread.h>
 #include <stdlib.h>
 
 #define WIN32_LEAN_AND_MEAN
@@ -43,6 +45,226 @@
 #define P32_CONTEXT_PC(context) (context).Pc
 #endif
 
+/*******************************************************************************
+ * Structures, functions and macros used to lookup functions at runtime.
+ */
+
+/**
+ * Windows 9x systems lack Unicode support; use `GetModuleHandleA` instead of
+ * `GetModuleHandleW`.
+ */
+#if P32_WIN9X
+#define P32GetModuleHandle(module) GetModuleHandleA (module)
+#else
+#define P32GetModuleHandle(module) GetModuleHandleW (TEXT (module))
+#endif
+
+/**
+ * Convenience wrapper for `GetProcAddress`.
+ */
+#define P32GetProcAddress(module, func) (Func##func) (UINT_PTR) GetProcAddress (module, #func)
+
+/**
+ * Suppress warnings about conversion between data and function pointers with
+ * picky compilers.
+ */
+#define F(ptr) (PVOID) (UINT_PTR) ptr
+
+/**
+ * Convenience wrapper for `InterlockedExchangePointer`.
+ */
+#define P32AtomicExchange(target, source) InterlockedExchangePointer ((void *volatile *) target, F (source))
+
+#if P32_WINNT < P32_WINNT_WIN7
+#define DYNAMIC_CHECKS
+
+/**
+ * Function type corresponding to `RaiseFailFastException`.
+ */
+typedef VOID (WINAPI *FuncRaiseFailFastException) (PEXCEPTION_RECORD, PCONTEXT, DWORD);
+#endif /* P32_WINNT < Windows 7 */
+
+#ifdef DYNAMIC_CHECKS
+/**
+ * Pointers to Windows APIs which are looked up at runtime.
+ */
+typedef struct RtApi {
+  pthread_once_t Init;
+
+  /**
+   * Function `RaiseFailFastException` is available since Windows 7.
+   */
+#if P32_WINNT < P32_WINNT_WIN7
+  FuncRaiseFailFastException PtrRaiseFailFastException;
+#endif /* P32_WINNT < Windows 7 */
+} RtApi;
+
+/**
+ * Windows APIs.
+ */
+static RtApi P32RtApi = {
+  .Init = PTHREAD_ONCE_INIT,
+
+#if P32_WINNT < P32_WINNT_WIN7
+  .PtrRaiseFailFastException = NULL,
+#endif /* P32_WINNT < Windows 7 */
+};
+
+/**
+ * Initialize `P32RtApi`.
+ *
+ * NOTE: we cannot call functions `p32_platform`, `p32_winnt` and `p32_win9x`;
+ *   they may call `p32_terminate` on error, which will result in a dead lock.
+ */
+static void P32RtApiInit (void) {
+  HMODULE kernel32 = P32GetModuleHandle ("kernel32.dll");
+  assert (kernel32 != NULL);
+
+#if P32_WINNT < P32_WINNT_WIN7
+  /**
+   * Check for `RaiseFailFastException`.
+   */
+  FuncRaiseFailFastException ptrRaiseFailFastException = NULL;
+
+  if (kernel32 != NULL) {
+    ptrRaiseFailFastException = P32GetProcAddress (kernel32, RaiseFailFastException);
+  }
+
+  if (ptrRaiseFailFastException != NULL) {
+    P32AtomicExchange (&P32RtApi.PtrRaiseFailFastException, ptrRaiseFailFastException);
+  }
+#endif /* P32_WINNT < Windows 7 */
+}
+#endif /* DYNAMIC_CHECKS */
+
+#if P32_WINNT < P32_WINNT_WIN7
+#define RaiseFailFastException P32RtApi.PtrRaiseFailFastException
+#endif /* P32_WINNT < Windows 7 */
+
+/*******************************************************************************
+ * Implementation.
+ *
+ * RuntimeRaiseFatalException:
+ *
+ *   <= Windows Vista
+ *
+ *   Raises a non-continuable exception (`EXCEPTION_NONCONTINUABLE`).
+ *   Such exception indicates that it is impossible to continue from the
+ *   current state and will likely terminate the process.
+ *
+ *   If the raised exception was handled and did not terminate the process,
+ *   call `TerminateProcess`.
+ *
+ *   >= Windows 7
+ *
+ *   Ues `RaiseFailFastException` to raise an exception which bypasses
+ *   ALL exception handlers and always terminates the process.
+ */
+
+#if P32_WINNT < P32_WINNT_WIN7
+/**
+ * Function corresponding to `RuntimeRaiseFatalException`.
+ */
+typedef void (*FuncRuntimeRaiseFatalException) (PCONTEXT);
+
+/**
+ * `RuntimeRaiseFatalException` implementation using `RaiseException`.
+ */
+static void P32RaiseNonContinuableException (PCONTEXT context);
+
+/**
+ * Initialization thunk for `RuntimeRaiseFatalException`.
+ */
+static void P32RuntimeRaiseFatalExceptionInit (PCONTEXT context);
+#endif /* P32_WINNT < Windows 7 */
+
+/**
+ * `RuntimeRaiseFatalException` implementation using `RaiseFailFastException`.
+ */
+static void P32RaiseFailFastException (PCONTEXT context);
+
+#ifdef DYNAMIC_CHECKS
+/**
+ * Pointers to implementation.
+ */
+typedef struct RuntimeApi {
+  pthread_once_t Init;
+
+#if P32_WINNT < P32_WINNT_WIN7
+  /**
+   * Implementation for `RuntimeRaiseFatalException`.
+   */
+  FuncRuntimeRaiseFatalException PtrRuntimeRaiseFatalException;
+#endif /* P32_WINNT < Windows 7 */
+} RuntimeApi;
+
+/**
+ * Implementation.
+ */
+static RuntimeApi P32RuntimeApi = {
+  .Init = PTHREAD_ONCE_INIT,
+
+#if P32_WINNT < P32_WINNT_WIN7
+  .PtrRuntimeRaiseFatalException = P32RuntimeRaiseFatalExceptionInit,
+#endif /* P32_WINNT < Windows 7 */
+};
+
+/**
+ * Initialize `P32RuntimeApi`.
+ */
+static void P32RuntimeApiInit (void) {
+  pthread_once (&P32RtApi.Init, P32RtApiInit);
+
+#if P32_WINNT < P32_WINNT_WIN7
+  if (P32RtApi.PtrRaiseFailFastException != NULL) {
+    P32AtomicExchange (&P32RuntimeApi.PtrRuntimeRaiseFatalException, P32RaiseFailFastException);
+  } else {
+    P32AtomicExchange (&P32RuntimeApi.PtrRuntimeRaiseFatalException, P32RaiseNonContinuableException);
+  }
+#endif /* P32_WINNT < Windows 7 */
+}
+#endif /* DYNAMIC_CHECKS */
+
+#if P32_WINNT < P32_WINNT_WIN7
+#define RuntimeRaiseFatalException P32RuntimeApi.PtrRuntimeRaiseFatalException
+
+static void P32RaiseNonContinuableException (PCONTEXT context) {
+  RaiseException (STATUS_FATAL_APP_EXIT, EXCEPTION_NONCONTINUABLE, 0, NULL);
+
+  _RPTW0 (_CRT_ERROR, L"Non-continuable exception failed to terminate process.\n");
+
+  /**
+   * If non-continuable exception was handled, terminate process the old way.
+   */
+  TerminateProcess (GetCurrentProcess (), STATUS_FATAL_APP_EXIT);
+
+  _RPTW0 (_CRT_ERROR, L"TerminateProcess failed to terminate process.\n");
+
+  return;
+  UNREFERENCED_PARAMETER (context);
+}
+
+static void P32RuntimeRaiseFatalExceptionInit (PCONTEXT context) {
+  pthread_once (&P32RuntimeApi.Init, P32RuntimeApiInit);
+  RuntimeRaiseFatalException (context);
+}
+#else /* P32_WINNT >= Windows 7 */
+#define RuntimeRaiseFatalException P32RaiseFailFastException
+#endif /* P32_WINNT >= Windows 7 */
+
+static void P32RaiseFailFastException (PCONTEXT context) {
+  EXCEPTION_RECORD exceptionRecord = {0};
+
+  exceptionRecord.ExceptionCode    = STATUS_FATAL_APP_EXIT;
+  exceptionRecord.ExceptionAddress = (PVOID) P32_CONTEXT_PC (*context);
+
+  RaiseFailFastException (&exceptionRecord, context, 0);
+}
+
+/*******************************************************************************
+ * External Functions.
+ */
+
 #ifdef LIBPOSIX32_TEST
 /**
  * Terminate handler set with `p32_terminate_handler`.
@@ -50,7 +272,7 @@
 static TerminateHandler P32TerminateHandler = NULL;
 
 void p32_terminate_handler (TerminateHandler handler) {
-  InterlockedExchangePointer ((volatile PVOID *) &P32TerminateHandler, (PVOID) (UINT_PTR) handler);
+  P32AtomicExchange (&P32TerminateHandler, handler);
 }
 #endif
 
@@ -68,30 +290,13 @@ void p32_terminate (const wchar_t *message, void *context) {
    */
   OutputDebugStringW (message);
 
-#if P32_WINNT >= P32_WINNT_WIN7
-  EXCEPTION_RECORD exceptionRecord = {0};
-
-  exceptionRecord.ExceptionCode    = STATUS_FATAL_APP_EXIT;
-  exceptionRecord.ExceptionAddress = (PVOID) P32_CONTEXT_PC (*(CONTEXT *) context);
-
-  RaiseFailFastException (&exceptionRecord, context, 0);
-#else
-  RaiseException (STATUS_FATAL_APP_EXIT, EXCEPTION_NONCONTINUABLE, 0, NULL);
-#endif
-
-  _RPTW0 (_CRT_ERROR, L"Runtime: non-continuable exception failed to terminate process.\n");
-
   /**
-   * Exception with EXCEPTION_NONCONTINUABLE flag should always terminate the
-   * process and we should never get here, but let's be a little paranoid.
+   * Terminate the process.
    */
-  TerminateProcess (GetCurrentProcess (), STATUS_FATAL_APP_EXIT);
-
-  _RPTW0 (_CRT_ERROR, L"Runtime: call to TerminateProcess failed to terminate process.\n");
+  RuntimeRaiseFatalException (context);
 
   /**
    * We should never ever get here.
    */
   _exit (STATUS_FATAL_APP_EXIT);
-  UNREFERENCED_PARAMETER (context);
 }
