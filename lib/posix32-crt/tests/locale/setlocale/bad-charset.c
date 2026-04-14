e382bd10b278d17f30ac117abd84f00e5a5dfaab
Kirill Makurin <maiddaisuki@outlook.com>
2026-04-15 00:12:08 +0900

    posix32-crt: tests: update locale/setlocale/bad-charset.c

--- lib/posix32-crt/tests/locale/setlocale/bad-charset.c
+++ lib/posix32-crt/tests/locale/setlocale/bad-charset.c
@@ -39,41 +39,68 @@
  * but it cannot be set for CRT's LC_CTYPE locale category.
  */
 
+typedef struct TestStrings {
+  /**
+   * Locale to set.
+   */
+  char *Locale;
+  /**
+   * Locale string expected to be returned by CRT's `[_w]setlocale`.
+   */
+#if P32_CRT >= P32_MSVCRT20
+  wchar_t *LocaleString;
+#else
+  char *LocaleString;
+#endif
+  /**
+   * Locale string expected to be returned by `getlocalename_l`.
+   */
+  char *LocaleName;
+} TestStrings;
+
 #if P32_CRT >= P32_MSVCRT20
 
 #define SETLOCALE(c, l) _wsetlocale (c, l)
 #define CMP(s1, s2)     wcscmp (s1, s2)
-#define STR(s)          TEXT (s)
 
 /**
  * "en-US" with its default ANSI code page the safest locale we can test.
  */
-#define LOCALE      "en-US.ACP"
-#define LOCALE_NAME "en-US.1252"
-#if (P32_LOCALE_API & P32_LOCALE_API_LN)
-#define LOCALE_STRING L"en-US"
-#else
-#define LOCALE_STRING L"English_United States.1252"
-#endif
+#define LOCALE_STRING     "en-US.ACP"
+#define LOCALE_NAME       "en-US.1252"
+#define LOCALE_CRT_STRING L"English_United States.1252"
+#define LOCALE_CRT_NAME   L"en-US"
 
 #else /* crtdll.dll or msvcrt10.dll */
 typedef const char *(*__cdecl setlocale_t) (int, const char *);
 
-#define SETLOCALE(c, l) crt_setlocale (c, l)
-#define CMP(s1, s2)     strcmp (s1, s2)
-#define STR(s)          s
+#define SETLOCALE(c, l)   crt_setlocale (c, l)
+#define CMP(s1, s2)       strcmp (s1, s2)
 
 /**
  * "en-US" with its default OEM code page the safest locale we can test.
  */
-#define LOCALE          "en-US.OCP"
-#define LOCALE_NAME     "en-US.437"
-#define LOCALE_STRING   "English_United States.437"
+#define LOCALE_STRING     "en-US.OCP"
+#define LOCALE_NAME       "en-US.437"
+#define LOCALE_CRT_STRING "English_United States.437"
+#define LOCALE_CRT_NAME   LOCALE_CRT_STRING
 
 #endif
 
+/**
+ * Locale strings for Global Locale.
+ */
+static TestStrings TestLocale;
+
 int main (void) {
   p32_test_init ();
+
+  /**
+   * Code page 10001 may not be supported/installed on old Windows versions.
+   */
+  if (!IsValidCodePage (10001)) {
+    return 77;
+  }
 
 #if P32_CRT <= P32_MSVCRT10
   setlocale_t crt_setlocale = NULL;
@@ -87,12 +114,21 @@ int main (void) {
   assert ((crt_setlocale = (setlocale_t) (UINT_PTR) GetProcAddress (crt, "setlocale")) != NULL);
 #endif
 
+  TestLocale.Locale     = LOCALE_STRING;
+  TestLocale.LocaleName = LOCALE_NAME;
+
+  if (P32_LOCALE_API & P32_LOCALE_API_LN) {
+    TestLocale.LocaleString = LOCALE_CRT_NAME;
+  } else {
+    TestLocale.LocaleString = LOCALE_CRT_STRING;
+  }
+
   /**
    * Set Global Locale.
    */
-  assert (setlocale (LC_ALL, LOCALE) != NULL);
-  assert (strcmp (getlocalename_l (LC_ALL, LC_GLOBAL_LOCALE), LOCALE_NAME) == 0);
-  assert (CMP (SETLOCALE (LC_ALL, NULL), LOCALE_STRING) == 0);
+  assert (setlocale (LC_ALL, TestLocale.Locale) != NULL);
+  assert (strcmp (getlocalename_l (LC_ALL, LC_GLOBAL_LOCALE), TestLocale.LocaleName) == 0);
+  assert (CMP (SETLOCALE (LC_ALL, NULL), TestLocale.LocaleString) == 0);
 
   /**
    * Attempt to set Global Locale locale with unsupported code page.
@@ -102,8 +138,8 @@ int main (void) {
   /**
    * Verify that Global Locale has not changed.
    */
-  assert (strcmp (getlocalename_l (LC_ALL, LC_GLOBAL_LOCALE), LOCALE_NAME) == 0);
-  assert (CMP (SETLOCALE (LC_ALL, NULL), LOCALE_STRING) == 0);
+  assert (strcmp (getlocalename_l (LC_ALL, LC_GLOBAL_LOCALE), TestLocale.LocaleName) == 0);
+  assert (CMP (SETLOCALE (LC_ALL, NULL), TestLocale.LocaleString) == 0);
 
   return EXIT_SUCCESS;
 }
