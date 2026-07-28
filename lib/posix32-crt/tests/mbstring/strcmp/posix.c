015843b35c623663cd38f3a87f0d830177d409f3
Kirill Makurin <maiddaisuki@outlook.com>
2026-07-28 21:10:24 +0900

    posix32-crt: tests: add tests for `p32_private_str[n]cmp_l` functions

---
+++ lib/posix32-crt/tests/mbstring/strcmp/posix.c
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2026 Kirill Makurin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#else
+#include "config-internal.h"
+#endif
+
+#include <assert.h>
+#include <stdlib.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "tests-internal.h"
+
+/**
+ * Test Summary:
+ *
+ * Test `p32_private_strcmp_l` function with ISO-8859-1 (code page 28591).
+ *
+ * This code page is used with "POSIX" locale.
+ */
+
+#define LOCALE "POSIX"
+static locale_t locale;
+
+/**
+ * Convenience macro to call `p32_private_strcmp_l`.
+ */
+#define strcmp(s1, s2) p32_private_strcmp_l (s1, s2, locale)
+
+static void DoTest (void) {
+  /**
+   * Previous converted Code Point.
+   */
+  char Prev[MB_LEN_MAX];
+  memset (Prev, 0, MB_LEN_MAX);
+
+  for (char32_t c32 = 0x01; c32 <= 0x0010FFFF; ++c32) {
+    /**
+     * Converted Code Point.
+     */
+    char Char[MB_LEN_MAX];
+    memset (Char, 0, MB_LEN_MAX);
+
+    size_t length = c32rtomb_l (Char, c32, NULL, locale);
+
+    if (length == (size_t) -1) {
+      continue;
+    }
+
+    int diff;
+
+    assert ((diff = strcmp ("", Char)) != _NLSCMPERROR);
+    assert (diff < 0);
+    assert ((diff = strcmp (Char, "")) != _NLSCMPERROR);
+    assert (diff > 0);
+
+    assert ((diff = strcmp (Prev, Char)) != _NLSCMPERROR);
+    assert (diff < 0);
+    assert ((diff = strcmp (Char, Prev)) != _NLSCMPERROR);
+    assert (diff > 0);
+
+    memcpy (Prev, Char, MB_LEN_MAX);
+  }
+}
+
+int main (void) {
+  p32_test_init ();
+
+  locale = newlocale (LC_ALL_MASK, LOCALE, NULL);
+  assert (locale != NULL);
+
+  DoTest ();
+
+  freelocale (locale);
+
+  return EXIT_SUCCESS;
+}
---
+++ lib/posix32-crt/tests/mbstring/strncmp/utf-8.c
@@ -0,0 +1,129 @@
+/**
+ * Copyright 2026 Kirill Makurin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#else
+#include "config-internal.h"
+#endif
+
+#include <assert.h>
+#include <stdlib.h>
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+
+#include "tests-internal.h"
+
+/**
+ * Test Summary:
+ *
+ * Test `p32_private_strncmp_l` function with UTF-8 (code page 65001).
+ */
+
+#define LOCALE "en_US.UTF-8"
+static locale_t locale;
+
+/**
+ * Convenience macro to call `p32_private_strncmp_l`.
+ */
+#define strncmp(s1, s2, n) p32_private_strncmp_l (s1, s2, n, locale)
+
+static void DoTest (void) {
+  /**
+   * Previous converted Code Point.
+   */
+  size_t PrevLength = 0;
+  char   Prev[MB_LEN_MAX];
+  memset (Prev, 0, MB_LEN_MAX);
+
+  for (char32_t c32 = 0x01; c32 <= 0x0010FFFF; ++c32) {
+    /**
+     * Converted Code Point.
+     */
+    char Char[MB_LEN_MAX];
+    memset (Char, 0, MB_LEN_MAX);
+
+    size_t length = c32rtomb_l (Char, c32, NULL, locale);
+    assert (PrevLength <= length);
+
+    if (length == (size_t) -1) {
+      continue;
+    }
+
+    int diff;
+
+    assert ((diff = strncmp (Prev, Char, 0)) != _NLSCMPERROR);
+    assert (diff == 0);
+    assert ((diff = strncmp (Char, Prev, 0)) != _NLSCMPERROR);
+    assert (diff == 0);
+
+    for (size_t i = 1; i < length; ++i) {
+      /**
+       * Compare multibyte sequence to an empty string; valid, non-zero-length
+       * multibyte sequence must always sort after an empty string.
+       */
+      assert ((diff = strncmp ("", Char, i)) != _NLSCMPERROR);
+      assert (diff < 0);
+      assert ((diff = strncmp (Char, "", i)) != _NLSCMPERROR);
+      assert (diff > 0);
+
+      /**
+       * Compare incomplete multibyte sequence to a complete multibyte sequence;
+       * complete multibyte sequence must always sort after incomplete one.
+       */
+      if (i == PrevLength) {
+        assert ((diff = strncmp (Prev, Char, i)) != _NLSCMPERROR);
+        assert (diff > 0);
+        assert ((diff = strncmp (Char, Prev, i)) != _NLSCMPERROR);
+        assert (diff < 0);
+        continue;
+      }
+
+      /**
+       * Compare one incomplete multibyte sequence to another;
+       * they must be sorted using binary order.
+       *
+       * FIXME: currently, two incomplete multibyte sequences compare equal.
+       */
+      assert ((diff = strncmp (Prev, Char, i)) != _NLSCMPERROR);
+      assert (diff == 0);
+      assert ((diff = strncmp (Char, Prev, i)) != _NLSCMPERROR);
+      assert (diff == 0);
+    }
+
+    assert ((diff = strncmp (Prev, Char, SIZE_MAX)) != _NLSCMPERROR);
+    assert (diff < 0);
+    assert ((diff = strncmp (Char, Prev, SIZE_MAX)) != _NLSCMPERROR);
+    assert (diff > 0);
+
+    memcpy (Prev, Char, MB_LEN_MAX);
+    PrevLength = length;
+  }
+}
+
+int main (void) {
+  p32_test_init ();
+
+  locale = newlocale (LC_ALL_MASK, LOCALE, NULL);
+  assert (locale != NULL);
+
+  DoTest ();
+
+  freelocale (locale);
+
+  return EXIT_SUCCESS;
+}
